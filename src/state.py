ee5b2170169293f46c86475fa4a6546b534854bf
Nyasha Chizampeni <nchizampeni@gmail.com>
2025-04-29 07:53:38 +0400

    Update state.py

--- src/state.py
+++ src/state.py
@@ -1,10 +1,20 @@
-
 class State:
     def __init__(self):
         self.state = {}
 
-    def get_state(self, id) -> str:
-        return self.state.get(id, None)
-    
-    def add_state(self, id, state) -> None:
+    def get_state(self, id: str) -> str | None:
+        """Retrieve the current state for a given id."""
+        return self.state.get(id)
+
+    def set_state(self, id: str, state: str) -> None:
+        """Set the state for a given id."""
         self.state[id] = state
+
+    def clear_state(self, id: str) -> None:
+        """Clear the state for a given id."""
+        if id in self.state:
+            del self.state[id]
+
+    def clear_all(self) -> None:
+        """Clear all states."""
+        self.state.clear()
