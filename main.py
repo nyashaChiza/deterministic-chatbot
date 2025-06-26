622cd9845410b911a7225a4acc50e427fc38979f
Nyasha Chizampeni <nchizampeni@gmail.com>
2025-06-26 15:57:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Union
 from fastapi import FastAPI
 from src.routes import webhook_router, callback_router
 
-app = FastAPI(title='Remittance Chatbot', description='Remittance Chatbot Backend', version='1.0.0') 
+app = FastAPI(title='Deterministic-Chatbot', description='A deterministic-chatbot Backend', version='1.0.0') 
 
 # Register routers
 app.include_router(webhook_router)
